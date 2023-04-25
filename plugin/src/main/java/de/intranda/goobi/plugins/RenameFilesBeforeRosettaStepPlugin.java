f1384c5dfb94db0eb56aeb8f7a7ed74e6bfbf992
zehong <zehong.hu@intranda.com>
2023-04-25 18:59:43 +0200

    a sketch of the structure

--- plugin/src/main/java/de/intranda/goobi/plugins/RenameFilesBeforeRosettaStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/RenameFilesBeforeRosettaStepPlugin.java
@@ -1,5 +1,8 @@
 package de.intranda.goobi.plugins;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 /**
  * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
  *
@@ -20,8 +23,11 @@ package de.intranda.goobi.plugins;
  */
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.configuration.SubnodeConfiguration;
+import org.goobi.beans.Process;
 import org.goobi.beans.Step;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginReturnValue;
@@ -30,9 +36,19 @@ import org.goobi.production.enums.StepReturnValue;
 import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
 
 import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.StorageProvider;
+import de.sub.goobi.helper.StorageProviderInterface;
+import de.sub.goobi.helper.exceptions.SwapException;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
+import ugh.dl.ContentFile;
+import ugh.dl.DigitalDocument;
+import ugh.dl.FileSet;
+import ugh.dl.Fileformat;
+import ugh.exceptions.PreferencesException;
+import ugh.exceptions.ReadException;
+import ugh.exceptions.WriteException;
 
 @PluginImplementation
 @Log4j2
@@ -48,6 +64,18 @@ public class RenameFilesBeforeRosettaStepPlugin implements IStepPluginVersion2 {
     private boolean allowTaskFinishButtons;
     private String returnPath;
 
+    private Process process;
+
+    private StorageProviderInterface storageProvider = StorageProvider.getInstance();
+
+    private String newFileNamePrefix;
+
+    private String derivateFolder;
+    private String altoFolder;
+    private String txtFolder;
+    private String pdfFolder;
+    private String xmlFolder;
+
     @Override
     public void initialize(Step step, String returnPath) {
         this.returnPath = returnPath;
@@ -59,6 +87,105 @@ public class RenameFilesBeforeRosettaStepPlugin implements IStepPluginVersion2 {
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
         log.info("rename_files_before_rosetta step plugin initialized");
         log.debug("value = " + value);
+        
+        process = this.step.getProzess();
+        initializeNewFileNamePrefix();
+        try {
+            derivateFolder = process.getImagesTifDirectory(false);
+            altoFolder = process.getOcrAltoDirectory();
+            txtFolder = process.getOcrTxtDirectory();
+            pdfFolder = process.getOcrPdfDirectory();
+            xmlFolder = process.getOcrXmlDirectory();
+        } catch (IOException | SwapException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeNewFileNamePrefix() {
+        String processTitle = process.getTitel();
+        newFileNamePrefix = processTitle.substring(processTitle.indexOf("_") + 1);
+    }
+
+    @Override
+    public PluginReturnValue run() {
+        boolean successful = true;
+        // your logic goes here
+
+        log.info("rename_files_before_rosetta step plugin executed");
+
+        // 1. create a Map from old names to new names
+        Map<String, String> namesMap = createNamesMap();
+
+        // 2. rename files in each folder with help of this Map
+        renameFilesInFolder(derivateFolder, namesMap);
+
+        // 3. update the Mets file
+
+        if (!successful) {
+            return PluginReturnValue.ERROR;
+        }
+        return PluginReturnValue.FINISH;
+    }
+
+    private Map<String, String> createNamesMap() {
+        Map<String, String> namesMap = new HashMap<>();
+        List<Path> files = storageProvider.listFiles(derivateFolder);
+        for (Path file : files) {
+            String fileName = file.getFileName().toString();
+            String oldName = fileName.substring(0, fileName.lastIndexOf("."));
+            // get new name based on this old name
+            String newName = newFileNamePrefix + "_" + oldName;
+            namesMap.put(oldName, newName);
+        }
+
+        return namesMap;
+    }
+
+    private boolean renameFilesInFolder(String folder, Map<String, String> namesMap) {
+        List<Path> files = storageProvider.listFiles(folder);
+
+        for (Path file : files) {
+            // get new filename 
+            String fileName = file.getFileName().toString();
+            int suffixIndex = fileName.lastIndexOf(".");
+            String suffix = fileName.substring(suffixIndex);
+            String oldName = fileName.substring(0, suffixIndex);
+            String newName = namesMap.get(oldName);
+            String newFileName = newName.concat(suffix);
+            // get new file path
+            Path targetPath = file.getParent().resolve(newFileName);
+            log.debug("targetPath = " + targetPath);
+        }
+
+        return true;
+    }
+
+    private boolean updateMetsFile(Map<String, String> namesMap) {
+        try {
+            Fileformat fileformat = process.readMetadataFile();
+            DigitalDocument dd = fileformat.getDigitalDocument();
+            FileSet fileSet = dd.getFileSet();
+            List<ContentFile> filesList = fileSet.getAllFiles();
+            for (ContentFile file : filesList) {
+                String oldLocation = file.getLocation();
+                int fileNameStartIndex = oldLocation.lastIndexOf("/") + 1;
+                String locationPrefix = oldLocation.substring(0, fileNameStartIndex);
+                String oldName = oldLocation.substring(fileNameStartIndex);
+                String newName = namesMap.get(oldName);
+                String newLocation = locationPrefix.concat(newName);
+                file.setLocation(newLocation);
+            }
+
+            process.writeMetadataFile(fileformat);
+
+            return true;
+
+        } catch (ReadException | IOException | SwapException | PreferencesException | WriteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
@@ -71,7 +198,7 @@ public class RenameFilesBeforeRosettaStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_sample.xhtml";
+        return null;
     }
 
     @Override
@@ -105,15 +232,4 @@ public class RenameFilesBeforeRosettaStepPlugin implements IStepPluginVersion2 {
         return ret != PluginReturnValue.ERROR;
     }
 
-    @Override
-    public PluginReturnValue run() {
-        boolean successful = true;
-        // your logic goes here
-        
-        log.info("rename_files_before_rosetta step plugin executed");
-        if (!successful) {
-            return PluginReturnValue.ERROR;
-        }
-        return PluginReturnValue.FINISH;
-    }
 }
